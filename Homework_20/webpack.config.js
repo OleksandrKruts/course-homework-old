2f87f6790fffb1542ff9c6932bec54f5c71cf74c
OleksandrKruts <kruts704@gmail.com>
2026-04-18 19:58:40 +0200

    add old homeworks

---
+++ Homework_20/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');            
+
+
+module.exports = {
+    entry: "./src/js/index.js",
+    module: {
+        rules: [
+            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+            { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.bundle.js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title:  "Webpack",
+        template: path.resolve(__dirname, './src/template.html'),
+    }),
+    new CleanWebpackPlugin()
+    ],
+    mode: process.env.NODE_ENV === "production" ? "production" : "development",
+}
